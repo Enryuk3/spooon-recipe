e43dfca8173f1717fa8bfe543e22cd0edb704ace
Cristopher Guzmán <guzmncristopher@gmail.com>
2025-05-16 19:45:30 -0500

    :sparkles: config provider nuxt img

---
+++ providers/spoonacular.ts
@@ -0,0 +1,16 @@
+import type { ProviderGetImage } from '@nuxt/image'
+import { joinURL } from 'ufo'
+
+export const getImage: ProviderGetImage = (
+  src,
+  { modifiers = {}, baseURL = 'https://spoonacular.com/cdn' } = {},
+) => {
+  const sizes = [100, 250, 500]
+  const size = modifiers.width || modifiers.height
+  const nextBiggest = sizes.find(s => s >= size) || sizes[sizes.length - 1]
+  const prefix = `ingredients_${nextBiggest}x${nextBiggest}`
+
+  return {
+    url: joinURL(baseURL, prefix, src),
+  }
+}
